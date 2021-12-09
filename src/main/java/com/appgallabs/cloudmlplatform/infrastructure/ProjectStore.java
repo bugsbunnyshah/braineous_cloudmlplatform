3c0e1d0c82dfc45097b1bbebd245cba16e3c2950
babyboy <bugs.bunny.shah@gmail.com>
2021-12-09 17:20:15 -0500

    cloudml scripting

--- src/main/java/com/appgallabs/cloudmlplatform/infrastructure/ProjectStore.java
+++ src/main/java/com/appgallabs/cloudmlplatform/infrastructure/ProjectStore.java
@@ -26,12 +26,7 @@ class ProjectStore {
         String principal = tenant.getPrincipal();
         String databaseName = principal + "_" + "aiplatform";
 
-        //TODO:REMOVE_ME
-        databaseName = "-2061008798_aiplatform";
-
         MongoDatabase database = mongoClient.getDatabase(databaseName);
-
-        System.out.println("DATABASE: "+databaseName);
 
         MongoCollection<Document> collection = database.getCollection("project");
 
